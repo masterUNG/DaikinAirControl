0f0c1dcd6249a481e7a5402fee611631ba48505f
MasterUNG <phrombutr@gmail.com>
2018-05-23 13:56:11 +0700

    Check Space Finish

--- app/src/main/java/masterung/androidthai/in/th/daikinaircontrol/fragment/AddAirFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/daikinaircontrol/fragment/AddAirFragment.java
@@ -6,13 +6,18 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import masterung.androidthai.in.th.daikinaircontrol.MainActivity;
 import masterung.androidthai.in.th.daikinaircontrol.R;
+import masterung.androidthai.in.th.daikinaircontrol.utility.MyAlert;
 
-public class AddAirFragment extends Fragment{
+public class AddAirFragment extends Fragment {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -23,6 +28,48 @@ public class AddAirFragment extends Fragment{
 
 
     }   // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itmeSave) {
+//            To Do
+            checkAndSave();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkAndSave() {
+
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText ipAddressEditText = getView().findViewById(R.id.edtIPAddress);
+        EditText macAddressEditText = getView().findViewById(R.id.edtMacAddress);
+
+        String nameString = nameEditText.getText().toString().trim();
+        String ipAddressString = ipAddressEditText.getText().toString().trim();
+        String macAddressString = macAddressEditText.getText().toString().trim();
+
+        if (nameString.isEmpty() || ipAddressString.isEmpty()) {
+
+//            Have Space
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space", "Please Fill All Blank");
+
+        } else {
+
+//            No Space
+
+        }
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_add_air, menu);
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarAddAir);
@@ -38,6 +85,8 @@ public class AddAirFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+
+        setHasOptionsMenu(true);
 
     }
 
