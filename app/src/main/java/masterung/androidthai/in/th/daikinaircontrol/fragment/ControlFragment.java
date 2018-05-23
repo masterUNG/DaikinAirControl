e5888fcae719e5dc97f1ed74c433c5c8520996e0
MasterUNG <phrombutr@gmail.com>
2018-05-23 15:54:47 +0700

    Finish 23May

--- app/src/main/java/masterung/androidthai/in/th/daikinaircontrol/fragment/ControlFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/daikinaircontrol/fragment/ControlFragment.java
@@ -5,11 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.daikinaircontrol.MainActivity;
 import masterung.androidthai.in.th.daikinaircontrol.R;
 
 public class ControlFragment extends Fragment {
@@ -44,11 +46,32 @@ public class ControlFragment extends Fragment {
 //        GetValue Argument
         getValueArgument();
 
+//        Create Toolbar
+        createToolbar();
+
+
 //        OnOff Controller
         onOffController();
 
 
     }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarControl);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(nameString);
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(ipAddressString);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
 
     private void getValueArgument() {
         idString = getArguments().getString("id");
